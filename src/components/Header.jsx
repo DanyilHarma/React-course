dbff2ca681508e78f896dbfa28b09204fa5c9df7
Danyil <danilagarma1999@mail.ru>
2024-06-29 16:33:28 +0300

    start-react

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://static.vecteezy.com/system/resources/thumbnails/027/971/388/small_2x/3d-render-round-warm-orange-fire-flame-icon-realistic-hot-sparks-light-gas-logo-design-for-emoticon-energy-power-ui-png.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="image-top">
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                <div>My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
