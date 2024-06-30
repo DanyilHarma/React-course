cec8fb267bc0ea4e9d0e8dd647c394e5bd0382ae
Danyil <danilagarma1999@mail.ru>
2024-07-01 00:27:47 +0300

    lesson 20

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,25 @@
+import { NavLink } from "react-router-dom";
 import classes from "./navbar.module.css"
 
 const Nav = () => {
     return (
         <nav className={classes.nav}>
-            <div><a href="/profile">Profile</a></div>
-            <div><a href="/dialogs">Messages</a></div>
-            <div><a href="/news">News</a></div>
-            <div><a href="/music">Music</a></div>
-            <div><a href="/settings">Settings</a></div>
-        </nav>
+            <div className={classes.item}>
+                <NavLink to="/profile" className={({ isActive }) => isActive ? classes.active : undefined}>Profile</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/dialogs" className={({ isActive }) => isActive ? classes.active : undefined}>Messages</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/news" className={({ isActive }) => isActive ? classes.active : undefined}>News</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/music" className={({ isActive }) => isActive ? classes.active : undefined}>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/settings" className={({ isActive }) => isActive ? classes.active : undefined}>Settings</NavLink>
+            </div>
+        </nav >
     )
 }
 
