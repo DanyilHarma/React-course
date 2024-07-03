acdd9ad827890446d59e5cf3e42d7d92f5f5d1b1
Danyil <danilagarma1999@mail.ru>
2024-07-04 00:59:36 +0300

    lesson 25

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,23 +1,14 @@
 import MyPosts from "./myPosts/MyPosts";
 import classes from "./profile.module.css"
+import ProfileInfo from "./profileInfo/profileInfo";
 
 const Profile = () => {
+
+
+
     return (
         <div>
-            <div className={classes.imageTop}>
-            </div>
-            <div>
-                <div className={classes.infoUser}>
-                    <div className={classes.avatar}> <img src="https://sm.ign.com/ign_ap/cover/a/avatar-gen/avatar-generations_hugw.jpg" alt="" /></div>
-                    <div className={classes.description}>
-                        <div>Avatar A.</div>
-                        <div>Date of Birth: 23 July</div>
-                        <div>City: Dubassary</div>
-                        <div>Education: Samurai</div>
-                        <div>Website: nope)</div>
-                    </div>
-                </div>
-            </div>
+            <ProfileInfo />
             <MyPosts />
         </div>
     )
