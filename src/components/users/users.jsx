cca0f02d9cf38269e3f676fdb893cee105788d24
Danyil <danilagarma1999@mail.ru>
2024-08-01 12:11:27 +0300

    lesson 57

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,59 +1,41 @@
-
-import axios from "axios";
 import UserContainer from "./user/userContainer";
 import classes from "./users.module.css"
-import { Component } from "react";
 
-class Users extends Component {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    setCurrentPage = (page) => {
-        this.props.setCurrentPage(page);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
-    }
-
-    render = () => {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div className={classes.usersAll}>
-                <div className={classes.buttonSelect}>
-                    {pages.map(page => {
-                        return <span onClick={(event) => { this.setCurrentPage(page) }} className={this.props.currentPage === page ? classes.pageSelected : classes.page}>{page}</span>
-                    })}
-                </div>
-                <h1>Users</h1>
-                <div className={classes.userContent}>
-                    {this.props.users.map(user => (
-                        <UserContainer
-                            key={user.id}
-                            id={user.id}
-                            name={user.name}
-                            imgSrc={user.imgSrc}
-                            city={user.city}
-                            country={user.country}
-                            messages={user.messages}
-                            isFollowed={user.isFollowed}
-                        />
-                    ))}
-                    (
-                    )
-                </div>
-            </div >
-        )
-    }
+    return (
+        <div className={classes.usersAll}>
+            <div className={classes.buttonSelect}>
+                {pages.map(page => {
+                    return <span onClick={(event) => { props.setCurrentPage(page) }} className={props.currentPage === page ? classes.pageSelected : classes.page}>{page}</span>
+                })}
+            </div>
+            <h1>Users</h1>
+            <div className={classes.userContent}>
+                {props.users.map(user => (
+                    <UserContainer
+                        key={user.id}
+                        id={user.id}
+                        name={user.name}
+                        imgSrc={user.photos.small}
+                        city={user.city}
+                        country={user.country}
+                        messages={user.messages}
+                        isFollowed={user.isFollowed}
+                    />
+                ))}
+                (
+                )
+            </div>
+        </div >
+    )
 }
 
 export default Users;
--- src/components/users/usersContainer.jsx
+++ src/components/users/usersContainer.jsx
@@ -1,18 +1,54 @@
 import { connect } from "react-redux"
 import Users from "./users"
-import { setCurrentPageCreator, setUsersCreator } from "../../redux/usersReducer";
+import { setCurrentPageCreator, setIsFetchingCreator, setUsersCreator } from "../../redux/usersReducer";
+import { Component } from "react";
+import axios from "axios";
+import Preloader from "../common/preloader/preloader";
 
+
+
+class UsersAPIComponent extends Component {
+
+    componentDidMount() {
+        this.props.toggleIsFetching(true);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    setCurrentPage = (page) => {
+        this.props.toggleIsFetching(true);
+        this.props.setCurrentPage(page);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    render = () => {
+        return <>
+            {this.props.isFetching ? <Preloader /> : null}
+            <Users totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize} currentPage={this.props.currentPage} users={this.props.users}
+                setCurrentPage={this.setCurrentPage}
+            />
+        </>
+    }
+}
 
 const mapStateToProps = (state) => ({
     users: state.user.users,
     pageSize: state.user.pageSize,
     totalUsersCount: state.user.totalUsersCount,
-    currentPage: state.user.currentPage
+    currentPage: state.user.currentPage,
+    isFetching: state.user.isFetching
 })
 
 const mapDispatchToProps = (dispatch) => ({
     setUsers: (users) => dispatch(setUsersCreator(users)),
-    setCurrentPage: (pageNumber) => dispatch(setCurrentPageCreator(pageNumber))
+    setCurrentPage: (pageNumber) => dispatch(setCurrentPageCreator(pageNumber)),
+    toggleIsFetching: (isFetching) => dispatch(setIsFetchingCreator(isFetching))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
